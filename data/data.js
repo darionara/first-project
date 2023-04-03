2f19a9ff1bebba3f515c00213954380292de321e
Daria Ziobrowska <darianocon@gmail.com>
2023-04-03 12:24:24 +0200

    Make some changes after CR

---
+++ data/data.js
@@ -0,0 +1,98 @@
+window.products = [
+    {
+      title: 'dimpa',
+      altText: 'Torba',
+      description: 'Torba, 65x22x65 cm',
+      price: '14.99',
+      image: './images/dimpa-torba-przezroczysty.avif',
+      imageHover: './images/dimpa-torba-przezroczysty-2.avif'
+    },
+    {
+      title: 'antagen',
+      altText: 'Szczotka',
+      description: 'Szczotka do zmywania',
+      price: '3.99',
+      image: './images/antagen-szczotka-do-zmywania-bialy.avif',
+      imageHover: './images/antagen-szczotka-do-zmywania-bialy-2.avif'
+    },
+    {
+      title: 'fiskbo',
+      altText: 'Ramka',
+      description: 'Ramka, czarna',
+      price: '13.99',
+      image: './images/fiskbo-ramka-czarny.avif',
+      imageHover: './images/fiskbo-ramka-czarny-2.avif'
+    },
+    {
+      title: 'heat',
+      altText: 'Podstawka',
+      description: 'Podstawka, korek',
+      price: '1.99',
+      image: './images/heat-podstawka-korek.avif',
+      imageHover: './images/heat-podstawka-korek-2.avif'
+    },
+    {
+      title: 'ikea 365',
+      altText: 'Butelka',
+      description: 'Butelka na wodę',
+      price: '11.99',
+      image: './images/ikea-365-butelka-na-wode.avif',
+      imageHover: './images/ikea-365-butelka-na-wode-2.avif'
+    },
+    {
+      title: 'istad',
+      altText: 'Torebka strunowa',
+      description: 'Torebka strunowa',
+      price: '1.99',
+      image: './images/istad-torebka-strunowa.avif',
+      imageHover: './images/istad-torebka-strunowa-2.avif'
+    },
+    {
+      title: 'istad',
+      altText: 'Torebka strunowa',
+      description: 'Torebka strunowa, szara',
+      price: '1.99',
+      image: './images/istad-torebka-strunowa-szary.avif',
+      imageHover: './images/istad-torebka-strunowa-szary-2.avif'
+    },
+    {
+      title: 'istad',
+      altText: 'Torebka strunowa',
+      description: 'Torebka strunowa, brąz',
+      price: '1.99',
+      image: './images/istad-torebka-strunowa-wzor-brazowy.avif',
+      imageHover: './images/istad-torebka-strunowa-wzor-brazowy-2.avif'
+    },
+    {
+      title: 'kalas',
+      altText: 'Kubek',
+      description: 'Kubek, różne kolory',
+      price: '4.99',
+      image: './images/kalas-kubek-rozne-kolory.avif',
+      imageHover: './images/kalas-kubek-rozne-kolory-2.avif'
+    },
+    {
+      title: 'lugnare',
+      altText: 'Świeca',
+      description: 'Świeca zapachowa',
+      price: '6.99',
+      image: './images/lugnare-swieca-zapachowa.avif',
+      imageHover: './images/lugnare-swieca-zapachowa-2.avif'
+    },
+    {
+      title: 'oevermaett',
+      altText: 'Osłona żywności',
+      description: 'Osłona żywności',
+      price: '3.99',
+      image: './images/oevermaett-oslona-zywnosci.avif',
+      imageHover: './images/oevermaett-oslona-zywnosci-2.avif'
+    },
+    {
+      title: 'variera',
+      altText: 'Pudełko',
+      description: 'Pudełko, białe',
+      price: '10.99',
+      image: './images/variera-pudelko-bialy.avif',
+      imageHover: './images/variera-pudelko-bialy-2.avif'
+    }
+  ];
--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,89 @@
-const leftScrollButton = document.getElementById('back-button');
-const rightScrollButton = document.getElementById('forward-button');
-const scrollableArea = document.getElementById('carousel');
+const backButton = document.getElementById('back-button');
+const forwardButton = document.getElementById('forward-button');
+const carousel = document.getElementById('carousel-wrapper');
+const carouselSlides = document.getElementsByClassName('carousel__product');
 
-const scrollLeft = () => {
-    scrollableArea.scrollBy({ 
+/* Displaying all the products in the carousel using the data from the outer file */
+const getProductHTML = (product) => {
+  const [ integer, decimals ] = product.price.split('.');
+
+  return `
+  <div class="carousel__product">
+  <a href="#">
+    <div class="product__image-wrapper">
+      <img class="product__image" src="${product.image}" alt="${product.altText}">
+      <img class="product__image product__image--hover" src="${product.imageHover}" alt="${product.altText}">
+    </div>
+    <div class="product__info">
+      <h3 class="product__title">${product.title}</h3>
+      <p class="product__description">${product.description}</p>
+      <div class="product__price">
+        <span class="product__price-integer">${integer}</span>
+        <span class="product__price-decimals">,${decimals}</span>
+      </div>
+      <a href="#">
+        <span class="product__favorite material-symbols-outlined">
+          favorite
+        </span>
+      </a>
+    </div>
+  </a>
+  </div>
+  `
+};
+
+/* I'm using the += operator to append each product to the innerHTML property rather than overwriting it. 
+This allows to display all the products in the array rather than just the last one */
+products.forEach((product) => {
+  carousel.innerHTML += getProductHTML(product);
+});
+
+
+
+/* IT DOESN'T WORK... I THINK IT IS BECAUSE carouselSlides IS NOT AN ARRAY OF PRODUCTS,
+I AM RIGHT? */
+/* Showing and hiding the carousel buttons depending on the slide number */
+const updateCarouselButtons = (currentSlide) => {
+
+  // Show the forward button on all slides except the last slide
+  if (currentSlide < carouselSlides.length) {
+    forwardButton.style.display = "block";
+  } else {
+    forwardButton.style.display = "none";
+  }
+
+  // Show the previous button on all slides except the first slide
+  if (currentSlide > 1) {
+    backButton.style.display = "block";
+  } else {
+    backButton.style.display = "none";
+  }
+};
+/* I ALSO THINK USING onmouseover ISN'T THE BEST OPTION, BUT IF NOT, WHEN AND HOW THIS FUNCTION SHOULD BE INVOKED? */
+carousel.addEventListener('onmouseover', updateCarouselButtons);
+
+
+
+/* Using buttons to scroll the slides */
+const scrollRight = () => {
+    carousel.scrollBy({ 
         top: 0, 
         left: 240, 
         behavior: 'smooth' 
       });
 }
 
-const scrollRight = () => {
-    scrollableArea.scrollBy({ 
+const scrollLeft = () => {
+    carousel.scrollBy({ 
         top: 0,
         left: -240, 
         behavior: 'smooth' 
       });
 }
 
-leftScrollButton.addEventListener('click', scrollRight);
-rightScrollButton.addEventListener('click', scrollLeft);
+backButton.addEventListener('click', scrollLeft);
+forwardButton.addEventListener('click', scrollRight);
+
+
+
+
