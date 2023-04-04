95fff2200ee59fcd48e8e8151d066252e2fe73dc
Daria Ziobrowska <darianocon@gmail.com>
2023-04-04 13:17:37 +0200

    Make the buttons showing and hiding conditionally

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,38 @@
 const backButton = document.getElementById('back-button');
 const forwardButton = document.getElementById('forward-button');
 const carousel = document.getElementById('carousel-wrapper');
-const carouselSlides = document.getElementsByClassName('carousel__product');
+/* const carouselSlides = document.getElementsByClassName('carousel__product'); */
+
+/* Showing and hiding the carousel buttons depending on the relation between the .scrollWidth (the total width of the carousel) 
+and the .clientWidth (the width of the part of carousel the user actually sees) 
+- .scrollWidth -> the width of an element, including padding, excluding borders, scrollbars or margins
+- .offsetWidth -> the viewable width of an element in pixels, including padding, border and scrollbar, but not the margin
+- .clientWidth -> the viewable width of an element in pixels, including padding, but not the border, scrollbar or margin
+*/
+const showingButtons = () => { 
+  if (carousel.scrollWidth <= carousel.clientWidth) {  
+    forwardButton.style.display='none';
+    backButton.style.display='none'; 
+  } else {
+    // Show the back button on all slides except the first slide - when the scroll is on 0 position
+      if (carousel.scrollLeft == 0) {
+        backButton.style.display='none';
+      } else {
+        backButton.style.display='block'
+      }
+    // fullWidth is the width of the part of the carousel which user can't see before scrolling
+      const fullWidth = carousel.scrollWidth - carousel.offsetWidth /* - 1 */;
+    // Show the forward button on all slides except the last slide - when the scroll reaches the fullWidth
+      if (carousel.scrollLeft >= fullWidth) {
+        forwardButton.style.display='none';
+      } else {
+        forwardButton.style.display='block';
+      }
+    }
+  };
+
+carousel.addEventListener("scroll", showingButtons);
+
 
 /* Displaying all the products in the carousel using the data from the outer file */
 const getProductHTML = (product) => {
@@ -41,9 +72,9 @@ products.forEach((product) => {
 
 
 /* IT DOESN'T WORK... I THINK IT IS BECAUSE carouselSlides IS NOT AN ARRAY OF PRODUCTS,
-I AM RIGHT? */
+AM I RIGHT? */
 /* Showing and hiding the carousel buttons depending on the slide number */
-const updateCarouselButtons = (currentSlide) => {
+/* const updateCarouselButtons = (currentSlide) => {
 
   // Show the forward button on all slides except the last slide
   if (currentSlide < carouselSlides.length) {
@@ -58,17 +89,17 @@ const updateCarouselButtons = (currentSlide) => {
   } else {
     backButton.style.display = "none";
   }
-};
+}; */
 /* I ALSO THINK USING onmouseover ISN'T THE BEST OPTION, BUT IF NOT, WHEN AND HOW THIS FUNCTION SHOULD BE INVOKED? */
-carousel.addEventListener('onmouseover', updateCarouselButtons);
+/* carousel.addEventListener('onmouseover', updateCarouselButtons); */
 
 
 
-/* Using buttons to scroll the slides */
+/* Using buttons to scroll the slides of the value of the carousel.clientWidth */
 const scrollRight = () => {
     carousel.scrollBy({ 
         top: 0, 
-        left: 240, 
+        left: carousel.clientWidth, 
         behavior: 'smooth' 
       });
 }
@@ -76,7 +107,7 @@ const scrollRight = () => {
 const scrollLeft = () => {
     carousel.scrollBy({ 
         top: 0,
-        left: -240, 
+        left: - carousel.clientWidth, 
         behavior: 'smooth' 
       });
 }
