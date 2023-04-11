7d05c40152fbe4882cb63db99ca0f741ae2a812c
Daria Ziobrowska <darianocon@gmail.com>
2023-04-11 15:06:41 +0200

    Print a christmas tree in the console

--- js/tree.js
+++ js/tree.js
@@ -1,2 +1,25 @@
 
+const drawTree = () => {
+    // Hardcode
+    /*    let branches = ['\\*', '\\**', '\\***', '\\****', '\\*****'];
+      
+      for (let branch of branches) {
+          console.log(branch)
+        }
 
+    // OR
+        branches.forEach(branch => console.log(branch));
+    */
+    
+    // Is it ok?
+      let branches = [];
+      
+      for (let i = 0; i <= 4; i++) {
+        branches.push('\\');
+        let starsToAdd = '*'.repeat(i + 1);
+        branches[i] += starsToAdd;
+        console.log(branches[i]);
+      }
+     }
+
+     drawTree();
