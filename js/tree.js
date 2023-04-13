0c5ca8093d5a899ed513ce9750b1f87b49c61617
Daria Ziobrowska <darianocon@gmail.com>
2023-04-13 16:46:36 +0200

    Using self-invoking function to draw a tree

--- js/tree.js
+++ js/tree.js
@@ -3,16 +3,16 @@ const drawTree = () => {
       let branches = [];
 
     // Using the repeat() method
-  /*  for (let i = 0; i <= 4; i++) {
+   for (let i = 0; i <= 4; i++) {
         branches.push('\\');
         let starsToAdd = '*'.repeat(i + 1);
         branches[i] += starsToAdd;
         console.log(branches[i]);
       }
-  */
+ 
     
     // Using the nested loop
-      for (let i = 0; i <= 4; i++) {
+/*       for (let i = 0; i <= 4; i++) {
         branches.push('\\');
         let starsToAdd = '';
         for (let j = 0; j < i + 1; j++) {
@@ -20,7 +20,16 @@ const drawTree = () => {
         }
         branches[i] += starsToAdd;
         console.log(branches[i]);
-      }
-     }
+      } */
+     } 
 
      drawTree();
+
+     // another way - using self-invoking anonymous function
+
+     (function() {
+      for (let i = 0; i < 5; i++) {
+        let branch = '\\' + '*'.repeat(i + 1);
+        console.log(branch);
+      }
+    })()
